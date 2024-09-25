3b6dc0c3efdc7f1cb452e00bf59e8737f4a0ebaf
Asfand322 <asfandsheikh321@gmail.com>
2024-09-25 13:47:49 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// array is where data stored
+// more than 1 variable stores
+// represented by []
+
+var name = ["asfand",1,true,null]
+document.write(name[0])
+
+// it shows 0 as asfand , 1 as 1 , 2 as true , 3 as null
+// null means neither zero nor empty
+// it means the place is just filled
+
+// 2D ARRAY:
+
+var name1 = ["asfand",1,[1,2,3],true,null]
+document.write(name1[2])
+
+// it will show 2 as 1,2,3
+// we can add more arrays in it
+// it is also called multi-dimensional array
+
+var name1 = ["asfand",1,[1,2,3],true,null]
+document.write(name1[2][1])
+
+// means 2 in 1 so it will show 2 only.
+
+var name1 = ["asfand",1,[1,2,3,[1,2]],true,null]
+document.write(name1[2][3][0])
+
+//...............................................
+
+
